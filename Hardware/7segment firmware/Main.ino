f748292d1f44f42c6d6de29c06fdf37db34537a0
jaiv shah <jaiv.maurya@gmail.com>
2021-02-06 19:20:59 -0500

    Add STM32 to 7 segment firmware .ino and .fzz file.

---
+++ Hardware/7segment firmware/Main.ino
@@ -0,0 +1,59 @@
+
+#define A PA0
+#define B PA1
+#define C PA2
+#define D PA3
+#define E PA4
+#define F PA5
+#define G PA6
+
+
+void setup() {
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  
+
+}
+
+void loop() {
+  sevenSeg(0,1,1,1,1,1,1);   //   0
+  delay(500);
+  sevenSeg(0,0,0,0,1,1,0);  //  1
+  delay(500);
+  sevenSeg(1,0,1,1,0,1,1);  // 2
+  delay(500);
+  sevenSeg(1,0,0,1,1,1,1); // 3
+  delay(500);
+  sevenSeg(1,1,0,0,1,1,0); // 4
+  delay(500);
+  sevenSeg(1,1,0,1,1,0,1); // 5
+  delay(500);
+  sevenSeg(1,1,1,1,1,0,1);  // 6
+  delay(500);
+  sevenSeg(0,0,0,0,1,1,1); // 7
+  delay(500);
+  sevenSeg(1,1,1,1,1,1,1); // 8
+  delay(500);
+  sevenSeg(1,1,0,1,1,1,1); // 9
+  delay(500);
+  
+
+}
+
+
+void sevenSeg (int g, int f, int e, int d, int c, int b, int a)
+{
+  digitalWrite(A, a);
+  digitalWrite(B, b);
+  digitalWrite(C, c);
+  digitalWrite(D, d);
+  digitalWrite(E, e);
+  digitalWrite(F, f);
+  digitalWrite(G, g);
+  
+}
