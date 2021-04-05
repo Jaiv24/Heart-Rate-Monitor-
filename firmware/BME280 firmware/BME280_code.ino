8d08996b8bbaed152e880cf5537b3b8d91e0337f
jaiv shah <jaiv.maurya@gmail.com>
2021-04-05 15:22:40 -0400

    Change and updated BME280_code.ino file

--- firmware/BME280 firmware/BME280_code.ino
+++ firmware/BME280 firmware/BME280_code.ino
@@ -1,21 +1,3 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
-  See the LICENSE file for details.
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -56,7 +38,7 @@ void setup() {
     }
     
     Serial.println("-- Default Test --");
-    delayTime = 1000;
+    delayTime = 5000;
 
     Serial.println();
 }
@@ -69,22 +51,8 @@ void loop() {
 
 
 void printValues() {
-    Serial.print("Temperature = ");
+    Serial.print("");
     Serial.print(bme.readTemperature());
-    Serial.println(" *C");
+    Serial.println(" ");
 
-    Serial.print("Pressure = ");
-
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-
-    Serial.println();
 }
